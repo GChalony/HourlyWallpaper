2beaeb733908b166ef3dffd9f64e285c0e443449
Gregoire Chalony <gchalony@gmail.com>
2021-02-21 19:44:43 +0100

    Fix bash globbing

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Copy stuff to the right location
 
-cp HourlyWallpaper $HOME/.config/
+if [[ ! -f "$HOME/.config/HourlyWallpaper" ]]; then 
+	cp HourlyWallpaper $HOME/.config/
+fi
 cp wallchanger $HOME/.local/bin/
 
 sudo cp wallchanger.cron /etc/cron.d/wallchanger
+
